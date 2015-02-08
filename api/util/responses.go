677d8f715b34e1864252addf3c982b075583efd8
Edward Bramanti <edward@bramanti.org>
2015-02-08 00:31:32 -0800

    Set up basic response utils

---
+++ api/util/responses.go
@@ -0,0 +1,44 @@
+package util
+
+import (
+	"github.com/ant0ine/go-json-rest/rest"
+	"github.com/jadengore/Ricetta/api/types"
+)
+
+//
+// Library of responses for the Ricetta API.
+//
+
+func (u Util) SimpleJsonResponse(w rest.ResponseWriter, code int, message string) {
+	w.WriteHeader(code)
+	w.WriteJson(types.Json{
+		"response": message,
+	})
+}
+
+func (u Util) SimpleJsonReason(w rest.ResponseWriter, code int, message string) {
+	w.WriteHeader(code)
+	w.WriteJson(types.Json{
+		"reason": message,
+	})
+}
+
+//
+// Validator Responses
+//
+
+func (u Util) SimpleJsonValidationReason(w rest.ResponseWriter, code int, err []error) {
+	errorMessage := decodeValidatorErrors(err)
+	w.WriteHeader(code)
+	w.WriteJson(types.Json{
+		"reason": errorMessage,
+	})
+}
+
+func decodeValidatorErrors(err []error) []string {
+	errorMessage := make([]string, len(err))
+	for i := range err {
+		errorMessage[i] = err[i].Error()
+	}
+	return errorMessage
+}
---
+++ api/util/util.go
@@ -0,0 +1,3 @@
+package util
+
+type Util struct{}
