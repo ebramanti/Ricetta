5c3f9051641ef651922bb8fe30b53442d538b280
Edward Bramanti <edward@bramanti.org>
2015-02-13 00:03:12 -0800

    Added teardown for testing

--- api/test/main.js
+++ api/test/main.js
@@ -1,16 +1,47 @@
 var util  = require('util'),
     spawn = require('child_process').spawn,
-    api   = spawn("go", ["run", "main.go"], {cwd: "../"});
+    api; // This is the child process that starts the Go API.
 
-api.stdout.on('data', function (data) {
-  console.log("API: " + data);
-});
+function SetUpSuite() {
+    api = spawn("go", ["run", "main.go"], {cwd: "../"});
+    api.stdout.on('data', function (data) {
+        console.log("API: " + data);
+    });
 
-api.stderr.on('data', function (data) {
-  console.log("API: " + data);
-  api.kill();
-});
+    api.stderr.on('data', function (data) {
+        console.log("API: " + data);
+    });
 
-api.on('exit', function (code) {
-  console.log('child process exited with code ' + code);
-});
+    api.on('exit', function (code) {
+        console.log('child process exited with code ' + code);
+    });
+}
+
+function TearDownSuite() {
+    neo4j.connect('http://localhost:7474/db/data/', function (err, graph) {
+        if (err) {
+            throw err;
+        }
+        // do something with the graph
+        var query = [
+            "MATCH (n)",
+             "OPTIONAL MATCH (n)-[r]-()",
+             "DELETE n, r"
+        ];
+
+        graph.query(query.join('\n'), function(err, results) {
+            if (err) {
+                console.log(err);
+                console.log(err.stack);
+            } else {
+                console.log("Database cleaned, teardown complete.")
+            }
+        });
+
+    });
+    api.kill();
+}
+
+(function test() {
+    SetUpSuite();
+})();
