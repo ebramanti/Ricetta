c3de9db716d51a5ec4a4257b326affb151d70676
Edward Bramanti <edward@bramanti.org>
2015-02-13 00:07:44 -0800

    Added testing flag and improved teardown

--- api/test/main.js
+++ api/test/main.js
@@ -1,9 +1,10 @@
 var util  = require('util'),
+    neo4j = require('neo4j-js'),
     spawn = require('child_process').spawn,
     api; // This is the child process that starts the Go API.
 
 function SetUpSuite() {
-    api = spawn("go", ["run", "main.go"], {cwd: "../"});
+    api = spawn("go", ["run", "main.go", "-testing=true"], {cwd: "../"});
     api.stdout.on('data', function (data) {
         console.log("API: " + data);
     });
@@ -21,23 +22,22 @@ function TearDownSuite() {
     neo4j.connect('http://localhost:7474/db/data/', function (err, graph) {
         if (err) {
             throw err;
+        } else {
+            var query = [
+                "MATCH (n)",
+                "OPTIONAL MATCH (n)-[r]-()",
+                "DELETE n, r"
+            ];
+
+            graph.query(query.join('\n'), function(err, results) {
+                if (err) {
+                    console.log(err);
+                    console.log(err.stack);
+                } else {
+                    console.log("Database cleaned, teardown complete.")
+                }
+            });
         }
-        // do something with the graph
-        var query = [
-            "MATCH (n)",
-             "OPTIONAL MATCH (n)-[r]-()",
-             "DELETE n, r"
-        ];
-
-        graph.query(query.join('\n'), function(err, results) {
-            if (err) {
-                console.log(err);
-                console.log(err.stack);
-            } else {
-                console.log("Database cleaned, teardown complete.")
-            }
-        });
-
     });
     api.kill();
 }
