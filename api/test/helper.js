9ae3ce4ad9ec9348f513501e5970c1d67484a7e1
Edward Bramanti <edward@bramanti.org>
2015-03-01 12:04:52 -0800

    Added DB init query to test helper

--- api/test/helper.js
+++ api/test/helper.js
@@ -1,20 +1,44 @@
 var neo4j = require('neo4j');
 var db = new neo4j.GraphDatabase('http://localhost:7474');
 
+var queries = {
+    wipe: [
+        "MATCH (n)",
+        "OPTIONAL MATCH (n)-[r]-()",
+        "DELETE n, r"
+    ],
+    initialize: "CREATE (pc:PublicCurator {name: \"PublicCurator\"})"
+}
+
 var helper = {
     constants: {
         host: 'http://localhost:8228/v1',
         // Routes
         signupRoute: '/signup'
     },
-    cleanup: function() {
-        var query = [
-            "MATCH (n)",
-            "OPTIONAL MATCH (n)-[r]-()",
-            "DELETE n, r"
-        ];
+    cleanupAndInit: function() {
+        var clean = queries.wipe.join('\n')
+        var init  = queries.initialize;
 
-        db.query(query.join('\n'), function(err, results) {
+        db.query(clean, function(err, results) {
+            // A clean returns 0 rows,
+            // throw error if something returns.
+            if (results) {
+                throw err;
+            }
+        });
+
+        db.query(init, function(err, results) {
+            if (err) {
+                console.log("Init: " + err);
+                throw err;
+            }
+        });
+    },
+    cleanup: function() {
+        var query = queries.wipe.join('\n');
+
+        db.query(query, function(err, results) {
             if (err) {
                 throw err;
             }
