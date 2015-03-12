f814ece72c8d105fbaf3fa2d3db377d954f63f62
Edward Bramanti <edward@bramanti.org>
2015-03-11 18:12:54 -0700

    Moved user validation to type file

--- api/types/user.go
+++ api/types/user.go
@@ -1,7 +1,9 @@
 package types
 
 import (
+	"fmt"
 	"time"
+	"unicode/utf8"
 )
 
 //
@@ -30,4 +32,46 @@ type InitialUserAttributes struct {
 type UserLogin struct {
 	Handle   string
 	Password string
+}
+
+//
+// Validator Functions
+//
+
+func (v RicettaValidator) validateHandle(i interface{}) error {
+	handle := i.(string)
+	if handle == "" {
+		return fmt.Errorf("Required field for signup")
+	} else if utf8.RuneCountInString(handle) > v.Constants.MAX_HANDLE_LENGTH {
+		return fmt.Errorf("Too long, max length is %d", v.Constants.MAX_HANDLE_LENGTH)
+	} else if !v.Constants.HANDLE_REGEX.MatchString(handle) {
+		return fmt.Errorf(handle + " contains illegal characters")
+	} else {
+		return nil
+	}
+}
+
+func (v RicettaValidator) validateEmail(i interface{}) error {
+	email := i.(string)
+	if email == "" {
+		return fmt.Errorf("Required field for signup")
+	} else if !v.Constants.EMAIL_REGEX.MatchString(email) {
+		return fmt.Errorf(email + " is an invalid email")
+	} else {
+		return nil
+	}
+}
+
+func (v RicettaValidator) validatePassword(i interface{}) error {
+	password := i.(string)
+	passwordLen := utf8.RuneCountInString(password)
+	if password == "" {
+		return fmt.Errorf("Required field for signup")
+	} else if passwordLen < v.Constants.MIN_PASS_LENGTH {
+		return fmt.Errorf("Too short, minimum length is %d", v.Constants.MIN_PASS_LENGTH)
+	} else if passwordLen > v.Constants.MAX_PASS_LENGTH {
+		return fmt.Errorf("Too long, maximum length is %d", v.Constants.MAX_PASS_LENGTH)
+	} else {
+		return nil
+	}
 }
