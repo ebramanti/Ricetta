8633b0adf1ee9974d2bc4cee2d07b70b94e2280b
Edward Bramanti <edward@bramanti.org>
2015-05-02 19:38:20 -0700

    Modified controller for auth

--- web/src/static/js/app/app-controller.js
+++ web/src/static/js/app/app-controller.js
@@ -7,10 +7,13 @@ define(function(require, exports, module) {
     var Session = require('app/models/session').Session;
 
     var LandingLayout = require('app/layouts/landing-layout').LandingLayout;
+    var HomeLayout = require('app/layouts/home-layout').HomeLayout;
 
     var SignupView = require('app/views/signup-view').SignupView;
     var FooterView = require('app/views/footer-view').FooterView;
     var LoginView = require('app/views/login-view').LoginView;
+    var NavbarView = require('app/views/navbar-view').NavbarView
+
 
     var AppController = marionette.Controller.extend({
         initialize: function() {
@@ -24,12 +27,9 @@ define(function(require, exports, module) {
                 $.ajaxSetup({
                     headers: {'Authorization' : this.app.session.get('token')}
                 })
+                this.app.session = app.session;
                 // user is authed, redirect home
-                this.app.mainRegion.show(new HomeLayout({
-                    session: this.app.session
-                }));
-                // Initialize header view after logged in
-            this.app.session = app.session;
+                this.showHomeLayout();
             };
         },
         // Needed for AppRouter to initialize index route.
@@ -39,6 +39,19 @@ define(function(require, exports, module) {
                 this.showHomeLayout();
             } else {
                 this.app.mainRegion.show(new LandingLayout({
+                    session: this.app.session
+                }));
+            }
+        },
+
+        showHomeLayout:function() {
+            if (!this.app.session.hasAuth()) {
+                this.index();
+            } else {
+                this.app.headerRegion.show(new NavbarView({
+                    session: this.app.session
+                }));
+                this.app.mainRegion.show(new HomeLayout({
                     session: this.app.session
                 }));
             }
