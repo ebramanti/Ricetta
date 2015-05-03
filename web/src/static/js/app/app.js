f6a30b43c4d32c4d32b2fb5213af7fc1905c101d
Edward Bramanti <edward@bramanti.org>
2015-05-02 19:37:53 -0700

    Modified app regions

--- web/src/static/js/app/app.js
+++ web/src/static/js/app/app.js
@@ -21,7 +21,6 @@ define(function(require, exports, module) {
     app.addRegions({
         headerRegion: '#header',
         mainRegion: '#main',
-        sidebarRegion:'#sidebarMain'
     })
 
     app.addInitializer(function() {
