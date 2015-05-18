7b0c0d840ccc2692873e84fe31e67007062e32b4
Edward Bramanti <edward@bramanti.org>
2015-05-17 23:44:48 -0700

    Add steps to schema

--- Recipe/schema.js
+++ Recipe/schema.js
@@ -1,7 +1,8 @@
-var Joi = require('joi');
+var Joi = require('joi')
 
-var ONE_DAY_IN_MINUTES = 1440;
-var AT_LEAST_2_TAGS = Joi.array().items(Joi.string()).min(2);
+var StepSchema = require('./step').Schema
+
+var ONE_DAY_IN_MINUTES = 1440
 
 var Schema = module.exports = Joi.object().keys({
   author: Joi.string().regex(/[a-zA-Z0-9]{4,30}/).required(),
@@ -12,8 +13,12 @@ var Schema = module.exports = Joi.object().keys({
   // ingredients: ...,
   cookTime: Joi.number().min(0).max(ONE_DAY_IN_MINUTES),
   prepTime: Joi.number().min(0).max(ONE_DAY_IN_MINUTES),
-  // No steps for now
-  // steps: ...,
-  tags: AT_LEAST_2_TAGS,
-  isPrivate: Joi.boolean()
+  steps: Joi.array().items(
+    Joi.object().keys({
+        instruction: Joi.string().min(3).max(2048).required(),
+        time: Joi.number().min(1).max(ONE_DAY_IN_MINUTES)
+    })
+  ).min(1).required(),
+  tags: Joi.array().items(Joi.string()).min(1),
+  isPrivate: Joi.boolean().required()
 })
