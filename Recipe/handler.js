e53eab0548178f548f7ebd745fd1cf713ce58ca3
Edward Bramanti <edward@bramanti.org>
2015-05-18 23:53:16 -0700

    Added steps to NewRecipe handler
    We need testing!

--- Recipe/handler.js
+++ Recipe/handler.js
@@ -1,6 +1,8 @@
-var db = require('../connector')
 var uuid = require('uuid').v4
 var async = require('async')
+
+var db = require('../connector')
+var step = require('./step')
 
 var LABEL = 'Recipe'
 
@@ -8,23 +10,38 @@ module.exports = function (resource) {
 
   resource.NewRecipe = function (req, reply) {
     var recipe = req.payload
+    var steps = recipe.steps
     recipe.uuid = uuid()
+    recipe.steps = undefined
     async.waterfall([
       function createRecipeNode (callback) {
         db.save(recipe, function (err, node) {
-          if (err) { return reply(err, null); }
+          if (err) { return reply(err.message, null); }
           callback(null, node)
         })
       },
       function createRecipeLabel (node, callback) {
         db.label(node, [ LABEL ], function (err) {
-          if (err) { return reply(err, null); }
+          if (err) { return reply(err.message, null); }
           callback(null, node)
         })
       }
     ], function (error, node) {
-        return reply(recipe)
+        if (steps) {
+          console.log('Steps detected')
+          step.CreateSteps(node, steps, function(error) {
+            if (error) {
+              return reply('Unable to create steps')
+                .code(500)
+            }
+            recipe.steps = steps
+            return reply(recipe)
+              .code(201)
+          })
+        } else {
+          return reply(recipe)
           .code(201)
+        }
     })
   }
 
