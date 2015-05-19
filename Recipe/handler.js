f936c4b04f014dd5ce847503a2e923b48609db07
Edward Bramanti <edward@bramanti.org>
2015-05-18 22:22:38 -0700

    Move NewRecipe to async.waterfall

--- Recipe/handler.js
+++ Recipe/handler.js
@@ -1,5 +1,6 @@
 var db = require('../connector')
 var uuid = require('uuid').v4
+var async = require('async')
 
 var LABEL = 'Recipe'
 
@@ -8,15 +9,22 @@ module.exports = function (resource) {
   resource.NewRecipe = function (req, reply) {
     var recipe = req.payload
     recipe.uuid = uuid()
-    db.save(recipe, function (err, node) {
-      if (err) { return reply(err, null); }
-
-      db.label(node, [ LABEL ], function (err) {
-        if (err) { return reply(err, null); }
-
+    async.waterfall([
+      function createRecipeNode (callback) {
+        db.save(recipe, function (err, node) {
+          if (err) { return reply(err, null); }
+          callback(null, node)
+        })
+      },
+      function createRecipeLabel (node, callback) {
+        db.label(node, [ LABEL ], function (err) {
+          if (err) { return reply(err, null); }
+          callback(null, node)
+        })
+      }
+    ], function (error, node) {
         return reply(recipe)
           .code(201)
-      })
     })
   }
 
